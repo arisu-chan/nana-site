0616f0b938260b6bf071f97fdc0d53762e63d6f8
arisuchan <castromario456@gmail.com>
2019-02-10 19:05:40 -0200

    Jojo Todynho deliciosamente representada

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title>Home</title>
+</head>
+<body>
+	<table border="0" width="900px" align="center">
+		<tr>
+			<td><img src="img/nana_logo.png" alt="nana" width="240px" height="100px" /></td>
+			<td align="right">
+				<a href="index.php">Home |</a>
+				<a href="quem-somos.php">Quem Somos |</a>
+				<a href="contato.php">Contato</a> 
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="2">
+				<img src="img/tokyo_mirage.jpg" width="900px"/>
+			</td>
+		</tr>
+
+
+		<tr>
+			<td colspan="2">
+				<tr>
+					<td>
+						<img src="img/jojo_mo.jpg" width="450" height="250" />
+					</td>
+					<td><img src="img/jojo_mo1.jpeg" width="450" height="250" /></td>
+				</tr>
+				<tr>
+					<td>
+						<img src="img/jojo_tody.jpg" width="450" height="250" />
+					</td>
+					<td>
+						<img src="img/jojo_tod.jpg" width="450" height="250" />
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+						<img src="img/anita_jojo.jpg" width="450" height="250" />
+					</td>
+					<td>
+						<img src="img/lipo.jpg"  width="450" height="250" />
+					</td>
+				</tr>
+
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="2" align="center"><h4>Todos os direitos reservados</h4></td>
+		</tr>
+
+	</table>
+</body>
+</html>
---
+++ quem-somos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title>Quem Somos</title>
+</head>
+<body>
+	<table border="0" width="900px" align="center">
+		<tr>
+			<td><img src="img/nana_logo.png" alt="nana" width="250px" height="100px" /></td>
+			<td align="right">
+				<a href="index.php">Home |</a>
+				<a href="quem-somos.php">Quem Somos |</a>
+				<a href="contato.php">Contato |</a> 
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="2">
+				<img src="img/tokyo_mirage.jpg" width="900px"/>
+			</td>
+		</tr>
+
+
+		<tr>
+			<td colspan="2">
+				<p>
+				Jordana Gleise de Jesus Menezes (Rio de Janeiro, 11 de fevereiro de 1997), mais conhecida pelo nome artístico de Jojo Maronttinni ou Jojo Todynho, é uma cantora brasileira. Ficou nacionalmente conhecida após o lançamento do single "Que Tiro Foi Esse?".[1]</p>
+				<p>A artista nasceu e cresceu no subúrbio do Rio de Janeiro, no bairro de Bangu. Em busca de sua independência, mudou-se aos vinte anos para o bairro da Lapa, onde viveu em uma quitinete alugada até o início de 2018.[2] Na infância participava do coral de uma igreja, além de atuar em eventos de canto e dança no colégio. Oriunda de uma família muito humilde, perdeu o pai vítima de bala perdida ainda na infância. Para ajudar no sustento do lar, Jordana começou a trabalhar no início da adolescência, ocupando-se nas funções de telefonista, faxineira, camelô, cuidadora de idosos, babá em playground de shopping e até como vendedora de picolé.[3]</p>
+
+			</td>
+		</tr>
+
+		<tr>
+			<td colspan="2" align="center"><h4>Todos os direitos reservados</h4></td>
+		</tr>
+
+	</table>
+</body>
+</html>
